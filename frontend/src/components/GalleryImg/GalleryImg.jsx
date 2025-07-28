d165db03b46d80b50436aba1c677acf5dc4e5dfb
Andrei Davidovich <125674957+sarcasacras@users.noreply.github.com>
2025-07-28 11:43:14 +0200

    Design fixes + card overlay added

--- frontend/src/components/GalleryImg/GalleryImg.jsx
+++ frontend/src/components/GalleryImg/GalleryImg.jsx
@@ -1,9 +1,23 @@
-import "../GalleryImg/GalleryImg.css"
+import "../GalleryImg/GalleryImg.css";
+import { Link } from "react-router";
 
 export default function GalleryImg({ item }) {
   return (
-    <div className="galleryItem" style={{gridRowEnd: `span ${Math.ceil(item.height/100)}`}}>
-      <img src={item.link} alt="" className="galleryImg"/>
+    <div
+      className="galleryItem"
+      style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
+    >
+      <img src={item.link} alt="" className="galleryImg" />
+      <Link to={`/pins/${item.id}`} className="overlay"></Link>
+      <button className="saveButton">Save</button>
+      <div className="overlayButtons">
+        <button className="overlayButton">
+          <img src="icons/menu.svg" alt="" className="buttonImage"/>
+        </button>
+        <button className="overlayButton">
+          <img src="icons/download.svg" alt="" className="buttonImage" />
+        </button>
+      </div>
     </div>
   );
 }
