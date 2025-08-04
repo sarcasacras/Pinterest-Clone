7598760974b18e48c3fa7e6c9a916294a4c7cd84
Andrei Davidovich <125674957+sarcasacras@users.noreply.github.com>
2025-08-04 13:37:26 +0200

    PostPage basic design

---
+++ frontend/src/components/PostInteractions/PostInteractions.jsx
@@ -0,0 +1,17 @@
+import "./PostInteractions.css";
+import Img from "../Image/Image";
+
+export default function PostInteractions() {
+  return (
+    <div className="postInteractions">
+      <div className="interactionButtons">
+        <Img src="/icons/like.svg" alt="Like" className="buttonIcon" />
+        <Img src="/icons/share.svg" alt="Share" className="buttonIcon" />
+        <Img src="/icons/more.svg" alt="More" className="buttonIcon" />
+      </div>
+        <div className="saveButtonDiv">
+          <button className="savePost">Save</button>
+        </div>
+    </div>
+  );
+}
--- frontend/src/routes/PostPage/PostPage.jsx
+++ frontend/src/routes/PostPage/PostPage.jsx
@@ -1,5 +1,32 @@
+import "./PostPage.css";
+import Img from "../../components/Image/Image";
+import { Link } from "react-router";
+import PostInteractions from "../../components/PostInteractions/PostInteractions";
+
+
 export default function PostPage() {
   return (
-    <div>PostPage</div>
-  )
+    <div className="post-page">
+      <div className="post-container">
+        <div className="left">
+          <Img src={"/pins/pin4.jpg"} className="pin-image" w={376} />
+        </div>
+        <div className="right">
+          <div className="interactions">
+            <PostInteractions />
+          </div>
+          <div className="user-section">
+            <Link to="/john" className="user-profile">
+              <Img src="/general/no-avatar.svg" alt="User Avatar" className="user-avatar" />
+              <span className="username">John Doe</span>
+            </Link>
+          </div>
+          <div className="comments-section">
+            <h3>Comments</h3>
+          </div>
+          {/* <Comments /> */}
+        </div>
+      </div>
+    </div>
+  );
 }
